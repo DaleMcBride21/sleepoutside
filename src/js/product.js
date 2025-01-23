a6ab6fb49056aa37010baf16bc2eddc74682498d
DaleMcBride21 <dlmcburrito@gmail.com>
2025-01-23 14:45:00 -0700

    update

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,9 @@
-import { setLocalStorage } from "./utils.mjs";
+
 import { findProductById } from "./productData.mjs";
+import { setLocalStorage, getParam } from './utils.mjs';
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam('product');
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+console.log(findProductById(productId));
+
+
